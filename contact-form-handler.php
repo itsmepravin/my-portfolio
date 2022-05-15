834f3354e003d01b8889a49951a2904af3798978
itsmepravin <singh.pravin19@yahoo.com>
2022-05-15 20:15:31 +0545

    Changes to styles, fixed break points, completed contact page

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message']
 
-$email_from = 'info@xyzwebsite.com';
+$email_from = 'info@pravinportfolio2022.com';
 
 $email_subject = 'New Form Submission';
 
