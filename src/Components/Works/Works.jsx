1638391b751c9034824423f99071e3fd6ca57795
itsmepravin <singh.pravin19@yahoo.com>
2024-02-22 19:08:44 +0545

    Sonam Gear Website added

--- src/Components/Works/Works.jsx
+++ src/Components/Works/Works.jsx
@@ -5,6 +5,7 @@ import { BsArrowUpRightSquareFill } from "react-icons/bs";
 import bakingFlowerImg from "../../assets/bakingFlower.jpg";
 import myFashionNepalImg from "../../assets/myFashionNepal.jpg";
 import tanabMuktiHealingImg from "../../assets/tanabMuktiHealing.jpg";
+import sonamGearImg from "../../assets/sonamGear.jpg";
 
 export const Works = () => {
   return (
@@ -12,6 +13,50 @@ export const Works = () => {
       <div className="section">
         <h2 className="section__title different">Works</h2>
         <div className="allWorks">
+          <div className="works_container" data-aos="fade-right">
+            <div className="work">
+              <div className="work_videocontainer">
+                <div>
+                  <img
+                    src={sonamGearImg}
+                    alt="Sonam Gear Image"
+                    height={200}
+                    width={500}
+                  />
+                </div>
+              </div>
+              <div className="work_information">
+                <h2>Sonam Gear</h2>
+                <p>
+                  Not just a brand, Sonam Gear is a mission that helps, enable
+                  and support people to be the fittest, contented and most
+                  confident version of themselves.
+                </p>
+                <div>{/* Icons Here */}</div>
+                <div className="work_icons_container">
+                  <a
+                    href="https://sonamgear.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span style={{ borderRadius: "50%" }} type="button">
+                      <BsArrowUpRightSquareFill size="30px" />
+                    </span>
+                  </a>
+                  <a
+                    href="https://www.facebook.com/bakingflower"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span style={{ borderRadius: "50%" }} type="button">
+                      <FaFacebookF size="30px" />
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
           <div className="works_container" data-aos="fade-right">
             <div className="work">
               <div className="work_videocontainer">
